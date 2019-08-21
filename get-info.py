44f00bbfcd0133cd58b8c8b5ed27f8ff3aa0ad1e
sy <shi06yue@gmail.com>
2019-08-21 14:52:38 +0800

    加入自动从快代理获取代理信息！

--- get-info.py
+++ get-info.py
@@ -4,32 +4,46 @@ import random
 headers = {
 	"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36"
 }
-proxies = {
-	'https': '101.6.69.5:1081'
-}
+
 url = "https://api.bilibili.com/x/web-interface/archive/stat?aid="
-file = open("list.csv",'a')
+
+def getp():
+	return requests.get("http://dps.kdlapi.com/api/getdps/?orderid=976635491365392&num=1&pt=1&sep=1").text
 
 def get(urls): #,proxies=proxies
 	con = 0 # 错误计数
 	while True:
 		# 异常处理，如果被封IP会自动休息
 		try:
-			json = requests.get(urls,headers=headers,timeout=10).json()
+			json = requests.get(urls,headers=headers,timeout=3).json()
 		except:
 			if con == 5:
 				print("连续5次都失败了。建议更换代理。")
 				con = 0 # 归零
-				time.sleep(5)
+				# pro = getp()
+
+				# http_proxy =  'http://'+pro
+				# https_proxy = 'https://'+pro
+				
+				# proxies = {
+				# 	'http': http_proxy,
+				# 	'https': https_proxy,
+				# }
+				# time.sleep(2)
 			else:
 				print("出现了问题，可能是因为IP被限制。5秒后会重新尝试。")
 				con = con + 1
-				time.sleep(5)
+				# time.sleep(2)
 		else:
 			break
 	# print(json)
+
+	# 如果视频存在，写入
 	if(json['code'] == 0):
+		print("yes")
+		file = open("list.csv",'a')
 		data = json['data']
+		# 从上到下分别为：AV号,播放量,弹幕数,评论数,收藏数,硬币,分享,点赞
 		file.write(str(data['aid']) +',')
 		file.write(str(data['view'])+',')
 		file.write(str(data['danmaku'])+',')
@@ -38,13 +52,22 @@ def get(urls): #,proxies=proxies
 		file.write(str(data['coin'])+',')
 		file.write(str(data['share'])+',')
 		file.write(str(data['like']) + '\n')
-		# 从上到下分别为：AV号,播放量,弹幕数,评论数,收藏数,硬币,分享,点赞
+		# 需要关闭文件以保存
+		file.close()
 
-	
-n = 938316 # 从AV n 开始爬
+# 首先获取代理ip
+pro = getp()
+
+http_proxy =  'http://'+pro
+https_proxy = 'https://'+pro
+
+proxies = {
+	'http': http_proxy,
+	'https': https_proxy,
+}
+
+n = 2149426 # 从AV n 开始爬
 while True:
+	print(n)
 	get(url + str(n))
 	n = n+1
-	print(n)
-	# time.sleep(random.uniform(0,0.3))
-	
